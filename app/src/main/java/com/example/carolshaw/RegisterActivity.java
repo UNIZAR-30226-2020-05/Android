8933084e9f7925c367d1fd661b05bdd228f47018
Sergio <sergiomartinez_1997@hotmail.com>
2020-04-23 12:54:37 +0200

    Corrección de errores y vistas

--- app/src/main/java/com/example/carolshaw/RegisterActivity.java
+++ app/src/main/java/com/example/carolshaw/RegisterActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
     private TextView userLogin;
+    private TextView alias;
+    private TextView nombre;
+    private TextView apellidos;
+    private TextView password;
+    private TextView fecha;
+    private Button registrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +24,19 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         userLogin = findViewById(R.id.tvUserLogIn);
+        registrar = findViewById(R.id.btnRegister);
+        //Datos del usuario
+        alias = findViewById(R.id.Alias);
+        nombre = findViewById(R.id.nombre);
+        apellidos = findViewById(R.id.apellidos);
+        password = findViewById(R.id.password);
+        fecha = findViewById(R.id.fecha);
 
-        userLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-            }
-        });
+    }
+
+    public void cambiar(View v) {
+        String kk = alias.getText().toString();
+        Log.d("alias", kk);
+        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
     }
 }
