63a7bd1cf5355a172e4956cc8ea04b4567cea422
Sergio <sergiomartinez_1997@hotmail.com>
2020-05-06 11:28:16 +0200

    Creado UserRequest

--- app/src/main/java/com/example/carolshaw/objetos/UserRequest.java
+++ app/src/main/java/com/example/carolshaw/objetos/UserRequest.java
@@ -1,4 +1,67 @@
 package com.example.carolshaw.objetos;
 
 public class UserRequest {
+    private String nombre;
+    private String apellidos;
+    private String nick;
+    private String contrasena;
+    private Boolean tipo_user;
+    private String fecha_nacimiento;
+
+    public UserRequest(String nombre, String apellidos, String nick, String contrasena, String fecha_nacimiento) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.nick = nick;
+        this.contrasena = contrasena;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.tipo_user = false;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public Boolean getTipo_user() {
+        return tipo_user;
+    }
+
+    public void setTipo_user(Boolean tipo_user) {
+        this.tipo_user = tipo_user;
+    }
+
+    public String getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(String fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
 }
