9abe96e630ff84fb8099081c344bdf46e60e64ec
Sergio <sergiomartinez_1997@hotmail.com>
2020-05-06 12:11:45 +0200

    Conexión con bbdd correcta

--- app/src/main/java/com/example/carolshaw/RegisterActivity.java
+++ app/src/main/java/com/example/carolshaw/RegisterActivity.java
@@ -1,28 +1,39 @@
 package com.example.carolshaw;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.ArrayMap;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.carolshaw.objetos.User;
+import com.example.carolshaw.objetos.UserRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Map;
 
 
 public class RegisterActivity extends AppCompatActivity {
     public TextView userLogin;
-    private TextView alias;
+    private TextView nick;
     private TextView nombre;
     private TextView apellidos;
-    private TextView password;
+    private TextView contrasena;
     private TextView fecha;
     private Button registrar;
     private String URL_API = "http://3.22.247.114:8080";
 
-    private RequestQueue queue;
 
 
     @Override
@@ -33,53 +44,50 @@ public class RegisterActivity extends AppCompatActivity {
         userLogin = findViewById(R.id.tvUserLogIn);
         registrar = findViewById(R.id.btnRegister);
         //Datos del usuario
-        alias = findViewById(R.id.Alias);
+        nick = findViewById(R.id.Alias);
         nombre = findViewById(R.id.nombre);
         apellidos = findViewById(R.id.apellidos);
-        password = findViewById(R.id.password);
+        contrasena = findViewById(R.id.password);
         fecha = findViewById(R.id.fecha);
 
-        queue = Volley.newRequestQueue(this);
+        final RequestQueue rq = Volley.newRequestQueue(getApplicationContext());
 
-        /*Map<String, Object> params = new ArrayMap<>(2);
-        // Adding parameters to request
-        params.put(email, email);
-        params.put(Config.KEY_PASSWORD, password);
-
-        // Creating a JSON Object request
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(params),
-                new Response.Listener<JSONObject>()
-                {
-                    @Override
-                    public void onResponse(JSONObject response)
-                    {
-                        Log.d(TAG, response.toString());
-                        // other stuff ...
-                    }
-                },
-                new Response.ErrorListener()
-                {
-                    @Override
-                    public void onErrorResponse(VolleyError error)
-                    {
-                        // You can handle the error here if you want
-                    }
-                });
-
-        // Adding the string request to the queue
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(jsonObjectRequest);
-
-        queue.add(postRequest);
-        */
-
-        userLogin.setOnClickListener(new View.OnClickListener() {
+        registrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                User user = new User(nombre.getText().toString(), apellidos.getText().toString(),
-                        alias.getText().toString(),password.getText().toString(), fecha.getText().toString());
+                UserRequest user = new UserRequest(nombre.getText().toString(), apellidos.getText().toString(),
+                        nick.getText().toString(), contrasena.getText().toString(), fecha.getText().toString());
 
+                JSONObject params = new JSONObject();
+                // Adding parameters to request
+                try {
+                    params.put("nuevo", user);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
+                // Creating a JSON Object request
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URL_API + "/user/create", params,
+                        new Response.Listener<JSONObject>()
+                        {
+                            @Override
+                            public void onResponse(JSONObject response)
+                            {
+                                Log.d("response", "exito: " + response.toString());
+                                // other stuff ...
+                            }
+                        },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error)
+                            {
+                                Log.d("response", "error: " + error.toString());
+                            }
+                        });
+
+                // Adding the string request to the queue
+                rq.add(jsonObjectRequest);
             }
         });
 
