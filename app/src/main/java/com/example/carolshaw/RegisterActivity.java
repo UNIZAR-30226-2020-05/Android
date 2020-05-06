06503be16e6c341bcc96fda41c51790386a0f63b
Sergio <sergiomartinez_1997@hotmail.com>
2020-05-06 21:39:49 +0200

    esquema comprobacion de usuario

--- app/src/main/java/com/example/carolshaw/RegisterActivity.java
+++ app/src/main/java/com/example/carolshaw/RegisterActivity.java
@@ -1,12 +1,15 @@
 package com.example.carolshaw;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.ArrayMap;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,6 +20,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.carolshaw.objetos.UserRequest;
 
@@ -69,50 +73,103 @@ public class RegisterActivity extends AppCompatActivity {
                 UserRequest user = new UserRequest(nombre.getText().toString(), apellidos.getText().toString(),
                         nick.getText().toString(), contrasena.getText().toString(), fecha.getText().toString());
 
+                if (validar() && false) {
+                    JSONObject params = new JSONObject();
+                    // Adding parameters to request
+                    try {
+                        //params.put("nuevo", user);
+                        params.put("nombre", user.getNombre());
+                        params.put("apellidos", user.getApellidos());
+                        params.put("nick", user.getNick());
+                        params.put("contrasena", user.getContrasena());
+                        params.put("tipo_user", user.getTipo_user());
 
+                        //Hay que pasar la fecha parseada y en formato long
+                        DateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+                        Date date;
+                        date = df.parse(fecha.getText().toString());
+                        params.put("fecha_nacimiento", date.getTime());
+                    } catch (JSONException | ParseException e) {
+                        e.printStackTrace();
+                    }
 
-                JSONObject params = new JSONObject();
-                // Adding parameters to request
-                try {
-                    //params.put("nuevo", user);
-                    params.put("nombre", user.getNombre());
-                    params.put("apellidos", user.getApellidos());
-                    params.put("nick", user.getNick());
-                    params.put("contrasena", user.getContrasena());
-                    params.put("tipo_user", user.getTipo_user());
+                    // Creating a JSON Object request
+                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URL_API + "/user/create", params,
+                            new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    Log.d("response", "exito: " + response.toString());
+                                    // other stuff ...
+                                    Toast.makeText(RegisterActivity.this, "Registro realizado correctamente", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(RegisterActivity.this, MainLogged.class));
+                                }
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Log.d("response", "error: " + error.toString());
+                                }
+                            });
 
-                    //Hay que pasar la fecha parseada y en formato long
-                    DateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
-                    Date date;
-                    date = df.parse(fecha.getText().toString());
-                    params.put("fecha_nacimiento", date.getTime());
-                } catch (JSONException | ParseException e) {
-                    e.printStackTrace();
+                    // Adding the string request to the queue
+                    rq.add(jsonObjectRequest);
                 }
-
-                // Creating a JSON Object request
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URL_API + "/user/create", params,
-                        new Response.Listener<JSONObject>()
-                        {
-                            @Override
-                            public void onResponse(JSONObject response)
-                            {
-                                Log.d("response", "exito: " + response.toString());
-                                // other stuff ...
-                            }
-                        },
-                        new Response.ErrorListener()
-                        {
-                            @Override
-                            public void onErrorResponse(VolleyError error)
-                            {
-                                Log.d("response", "error: " + error.toString());
-                            }
-                        });
-
-                // Adding the string request to the queue
-                rq.add(jsonObjectRequest);
             }
         });
+    }
+    private boolean validar(){
+        Boolean valido = false;
+
+        //validating inputs
+        /*if (TextUtils.isEmpty(username)) {
+            editTextUsername.setError("Please enter your username");
+            editTextUsername.requestFocus();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            editTextPassword.setError("Please enter your password");
+            editTextPassword.requestFocus();
+            return;
+        }*/
+
+        final RequestQueue rq = Volley.newRequestQueue(getApplicationContext());
+        /*JSONObject params = new JSONObject();
+        // Adding parameters to request
+        try {
+            params.put("nick", nick.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }*/
+
+        StringRequest sr = new StringRequest(Request.Method.GET, URL_API + "/user/get",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("response", "exito validar: " + response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("response", "error validar: " + error.toString());
+                    }
+                })
+        {
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("nick","smm");
+                return params;
+            }
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/json");
+                return params;
+            }
+        };
+        rq.add(sr);
+        return valido;
     }
 }
