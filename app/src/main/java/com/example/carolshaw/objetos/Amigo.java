a8e0a4830f15ca7d204e4359fb99c5bef9468aa8
samuelosb <samuelosb@users.noreply.github.com>
2020-05-09 22:15:54 +0200

    Objeto base para lista de amigos

--- app/src/main/java/com/example/carolshaw/objetos/Amigo.java
+++ app/src/main/java/com/example/carolshaw/objetos/Amigo.java
@@ -1,4 +1,49 @@
 package com.example.carolshaw.objetos;
 
 public class Amigo {
+
+    private Integer id;
+    private String nick;
+    private String nombre;
+    private String apellidos;
+    private String ultimaCancion;
+
+    public Integer getId() {
+        return id;    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getUltimaCancion() {
+        return ultimaCancion;
+    }
+
+    public void setUltimaCancion(String ultimaCancion) {
+        this.ultimaCancion = ultimaCancion;
+    }
 }
