e80cc36a264e1b275a99f13d295028886fa375c2
sergio <sergiomartinez_1997@hotmail.com>
2020-05-17 01:52:26 +0200

    Añadida busqueda de Podcasts y arrreglos de vistas

--- app/src/main/java/com/example/carolshaw/AnadirPodcastALista.java
+++ app/src/main/java/com/example/carolshaw/AnadirPodcastALista.java
@@ -9,11 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AnadirPodcastALista#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AnadirPodcastALista extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
